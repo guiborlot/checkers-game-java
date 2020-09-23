0866cde839c56c17f1c4d92fc815766fd817cb5f
GUILHERME BORLOT OLIVEIRA <guiborlot@edu.unifil.br>
2020-09-23 14:05:46 -0300

    Created packages and classes, initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        CheckersMatch checkersMatch = new CheckersMatch();
+    }
+}
