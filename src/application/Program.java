a2f62db557541d26c3f2bc9d73a038f2071f2a25
GUILHERME BORLOT OLIVEIRA <guiborlot@edu.unifil.br>
2020-09-23 17:52:02 -0300

    Clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import checkers.CheckersMatch;
 import java.util.Scanner;
 import checkers.CheckersPiece;
 import checkers.CheckersPosition;
+import java.util.InputMismatchException;
+import checkers.CheckersException;
 
 
 public class Program {
@@ -14,16 +16,25 @@ public class Program {
         CheckersMatch checkersMatch = new CheckersMatch();
 
         while(true) {
-            UI.printBoard(checkersMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            CheckersPosition source = UI.readCheckersPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(checkersMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                CheckersPosition source = UI.readCheckersPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            CheckersPosition target = UI.readCheckersPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                CheckersPosition target = UI.readCheckersPosition(sc);
 
-            CheckersPiece capturedPiece = checkersMatch.performCheckersMove(source, target);
+                CheckersPiece capturedPiece = checkersMatch.performCheckersMove(source, target);
+            } catch (CheckersException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
