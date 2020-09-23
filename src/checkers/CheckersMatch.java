9d2810358a3ee17e87a3dd7c6b3713c0565b53ab
GUILHERME BORLOT OLIVEIRA <guiborlot@edu.unifil.br>
2020-09-23 17:58:06 -0300

    Possible moves of a piece

--- src/checkers/CheckersMatch.java
+++ src/checkers/CheckersMatch.java
@@ -44,6 +44,10 @@ public class CheckersMatch {
         if(!board.thereIsAPiece(position)) {
             throw new CheckersException("There is no piece on source position");
         }
+
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new CheckersException("There is no possible moves for the chosen piece");
+        }
     }
 
     private void placeNewPiece(char column, int row, CheckersPiece piece) {
--- src/checkers/CheckersPiece.java
+++ src/checkers/CheckersPiece.java
@@ -3,7 +3,7 @@ package checkers;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class CheckersPiece extends Piece {
+public abstract class CheckersPiece extends Piece {
     private Color color;
 
     // Passa pra classe Piece o board e recebe a cor das peças
